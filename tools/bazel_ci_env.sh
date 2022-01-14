cf73368f1c3e3fad2e48f2c394020c8f8e85158c
Toby Webb <t.webb@zaleos.net>
2022-01-14 18:29:37 +0100

    Fix branch tags to docker image tags translation

--- tools/bazel_ci_env.sh
+++ tools/bazel_ci_env.sh
@@ -5,4 +5,6 @@ echo "RANDOM_HASH $(cat /dev/urandom | head -c16 | md5sum 2>/dev/null | cut -f1 
 echo "STABLE_GIT_COMMIT $(git rev-parse HEAD)"
 echo "DOCKER_REGISTRY $DOCKER_REGISTRY"
 echo "DOCKER_REPOSITORY $DOCKER_REPOSITORY"
-echo "DOCKER_IMAGE_TAG $DOCKER_IMAGE_TAG"
+
+# Handle the '/' characters in branch names (and any non-alphanumeric characters):
+echo "DOCKER_IMAGE_TAG $(echo -n "$DOCKER_IMAGE_TAG" | tr -c [:alnum:] _)"
