95b0c2590baf366f4b19eb2799bc9965211ebcf8
Toby Webb <tobyjwebb@gmail.com>
2022-03-11 15:45:08 +0100

    Finish mocking APIs

--- src/web_frontend/api_battles.go
+++ src/web_frontend/api_battles.go
@@ -2,6 +2,7 @@ package web_frontend
 
 import (
 	"fmt"
+	"log"
 	"net/http"
 
 	"github.com/go-chi/chi/v5"
@@ -10,6 +11,8 @@ import (
 func (s *Server) setupBattlesRoutes() *chi.Mux {
 	challenges := chi.NewRouter()
 	challenges.Get("/{challenge_id}/state", s.getBatleStateHandler)
+	challenges.Get("/{challenge_id}/log", s.getBatleLogHandler)
+	challenges.Post("/{challenge_id}/move", s.postBatleMoveHandler)
 	return challenges
 }
 
@@ -19,10 +22,28 @@ func (s *Server) getBatleStateHandler(rw http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(rw, `{
 		"board": "          (XXX 64-chars, one for each pos in board)             ",
 		"turn":"white",
-		"latest_logs": [
+		"latest_movements": [
 			{"n": 5, "who":"user1", "piece":"q", "from": "A5", "to":"C6", "timestamp":"2022-02-22T11:11:11Z"},
 			{"n": 4, "who":"user2", "piece":"P", "from": "A5", "to":"C6", "timestamp":"2022-02-22T11:11:11Z"},
 			{"n": 3, "who":"user6", "piece":"k", "from": "A5", "to":"C6", "timestamp":"2022-02-22T11:11:11Z"}
 		]
 	}`)
+}
+
+func (s *Server) getBatleLogHandler(rw http.ResponseWriter, r *http.Request) {
+	// XXX implement getBatleLogHandler
+	setJSON(rw)
+	fmt.Fprintf(rw, `{"latest_movements":[
+			{"n": 5, "who":"user1", "piece":"q", "from": "A5", "to":"C6", "timestamp":"2022-02-22T11:11:11Z"},
+			{"n": 4, "who":"user2", "piece":"P", "from": "A5", "to":"C6", "timestamp":"2022-02-22T11:11:11Z"},
+			{"n": 3, "who":"user6", "piece":"k", "from": "A5", "to":"C6", "timestamp":"2022-02-22T11:11:11Z"}
+		]}`)
+}
+
+func (s *Server) postBatleMoveHandler(rw http.ResponseWriter, r *http.Request) {
+	// XXX implement postBatleMoveHandler
+	from := r.FormValue("from")
+	to := r.FormValue("to")
+	log.Println("XXX move from", from, "to", to) // XXX cleanup log
+	setJSON(rw)
 }
