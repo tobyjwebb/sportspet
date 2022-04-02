9b4e0fd9f527a2e70d7cb06dccc10d74cedcd271
Toby Webb <tobyjwebb@gmail.com>
2022-04-02 19:04:16 +0200

    Get battle status

--- src/web_frontend/api_battles.go
+++ src/web_frontend/api_battles.go
@@ -6,30 +6,53 @@ import (
 	"net/http"
 
 	"github.com/go-chi/chi/v5"
+	"github.com/tobyjwebb/teamchess/src/battles"
 )
 
 func (s *Server) setupBattlesRoutes() *chi.Mux {
 	challenges := chi.NewRouter()
-	challenges.Get("/{challenge_id}/state", s.getBatleStateHandler)
-	challenges.Get("/{challenge_id}/log", s.getBatleLogHandler)
-	challenges.Post("/{challenge_id}/move", s.postBatleMoveHandler)
+	challenges.Get("/{battle_id}/state", s.getBatleStateHandler)
+	challenges.Get("/{battle_id}/log", s.getBatleLogHandler)
+	challenges.Post("/{battle_id}/move", s.postBatleMoveHandler)
 	return challenges
 }
 
 func (s *Server) getBatleStateHandler(rw http.ResponseWriter, r *http.Request) {
-	// XXX implement getBatleStateHandler
+	battleID := chi.URLParam(r, "battle_id")
+	log.Println("Got battle id", battleID)
+
+	status, battle, err := s.doGetBattleState(battleID)
+	rw.WriteHeader(status)
+	if err != nil {
+		return
+	}
+
+	// TODO: Implement latest movements?:
+	// "latest_movements": [
+	// 	{"n": 5, "who":"user1", "piece":"q", "from": "A5", "to":"C6", "timestamp":"2022-02-22T11:11:11Z"},
+	// 	{"n": 4, "who":"user2", "piece":"P", "from": "A5", "to":"C6", "timestamp":"2022-02-22T11:11:11Z"},
+	// 	{"n": 3, "who":"user6", "piece":"k", "from": "A5", "to":"C6", "timestamp":"2022-02-22T11:11:11Z"}
+	// ]
+
+	turn := "black"
+	if battle.MoveCount%2 == 0 {
+		turn = "white"
+	}
 	setJSON(rw)
 	fmt.Fprintf(rw, `{
 		"board": "%s",
-		"white_team": "aabbcc-dd-11-33322323232233",
-		"black_team": "9999999ff-12332-23k4234j233",
-		"turn":"black",
-		"latest_movements": [
-			{"n": 5, "who":"user1", "piece":"q", "from": "A5", "to":"C6", "timestamp":"2022-02-22T11:11:11Z"},
-			{"n": 4, "who":"user2", "piece":"P", "from": "A5", "to":"C6", "timestamp":"2022-02-22T11:11:11Z"},
-			{"n": 3, "who":"user6", "piece":"k", "from": "A5", "to":"C6", "timestamp":"2022-02-22T11:11:11Z"}
-		]
-	}`, initialBoardStatus)
+		"white_team": "%s",
+		"black_team": "%s",
+		"turn":"%s",
+	}`, battle.Board, battle.WhiteTeamID, battle.BlackTeamID, turn)
+}
+
+func (s *Server) doGetBattleState(battleID string) (status int, battle *battles.Battle, err error) {
+	b, err := s.BattleService.GetData(battleID)
+	if err != nil {
+		return http.StatusInternalServerError, nil, err
+	}
+	return http.StatusOK, b, nil
 }
 
 func (s *Server) getBatleLogHandler(rw http.ResponseWriter, r *http.Request) {
