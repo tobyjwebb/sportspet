4de85cca3e62ddfb3fa0e7ef9c9241c24e7d6308
Toby Webb <tobyjwebb@gmail.com>
2022-04-22 17:21:41 +0200

    Don't allow teams to play out of turn

--- src/web_frontend/html/js/battle.js
+++ src/web_frontend/html/js/battle.js
@@ -1,3 +1,6 @@
+var currentTeamBlack = false;
+var currentTeamWhite = false;
+let current_team_turn = false;
 $(function () {
     var $board = $('#chessboard');
     // We need to get the current team colour before drawing the board,
@@ -7,15 +10,23 @@ $(function () {
         getBattleStatus(currentBattle).then(res => {
             if (currentTeamID == res.black_team) {
                 inverted = true;
+                currentTeamBlack = true;
+            } else if (currentTeamID == res.white_team) {
+                currentTeamWhite = true;
             }
             drawChessboard($board, inverted);
             refreshBoardStatusPeriodic();
         });
-    })
+    });
 
     $board.on('click', 'td.black, td.white', function () {
         var $this = $(this);
-        if (!movement && $this.text() == ' ') {
+        let piece = $this.text();
+        if (!current_team_turn) {
+            return;
+        }
+        // If starting movement, and cannot move piece, do nothing.
+        if (!movement && !canMovePiece(piece)) {
             return;
         }
         var cellID = $this.data('cell');
@@ -122,19 +133,44 @@ function refreshBoardStatus() {
 }
 
 function updateTurnStatus(boardStatus) {
-    let your_turn = true;
     var $battle_status = $('#battle_status');
     if (boardStatus.black_team == currentTeamID && boardStatus.turn == "black") {
         $battle_status.text("Your team's turn!");
+        current_team_turn = true;
     } else if (boardStatus.white_team == currentTeamID && boardStatus.turn == "white") {
         $battle_status.text("Your team's turn!");
+        current_team_turn = true;
     } else {
         $battle_status.text(`Team ${boardStatus.turn}'s turn.`);
-        your_turn = false;
+        current_team_turn = false;
     }
-    if (your_turn) {
+    if (current_team_turn) {
         $('#chessboard').addClass('your-turn');
     } else {
         $('#chessboard').removeClass('your-turn');
     }
+}
+
+function sprite2letter(sprite) {
+    for (let letter in pieceSprites) {
+        if (pieceSprites[letter] == sprite) {
+            return letter;
+        }
+    }
+    return ' ';
+}
+
+function canMovePiece(p) {
+    // p is a sprite, let's check the letter it represents to check if it is a lower or upper case:
+    p = sprite2letter(p);
+    if (p == ' ') {
+        return false;
+    }
+    if (currentTeamWhite && p.toUpperCase() == p) {
+        return true;
+    }
+    if (currentTeamBlack && p.toLowerCase() == p) {
+        return true;
+    }
+    return false;
 }
